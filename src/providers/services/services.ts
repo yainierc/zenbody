e167a65b7640c8f887f9d7fdef7f26441c603b80
yainierc <yainierc@yahoo.es>
2017-07-31 00:28:30 -0400

    Update services.ts

--- src/providers/services/services.ts
+++ src/providers/services/services.ts
@@ -25,7 +25,7 @@ export class ServicesProvider {
     return new Promise(resolve => {
 
       //this.http.get('http://107.180.65.249:3000/api/schedules')
-      this.http.get('http://68.66.193.239:3000/api/services')      
+      this.http.get('http://192.241.155.179:3000/api/services')      
         .map(res => res.json())
         .subscribe(data => {
           this.data = data;
